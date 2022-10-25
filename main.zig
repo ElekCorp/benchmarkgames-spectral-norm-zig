f4b7fc04b349d1122812d9ee7c8249e01a53cf1f
ElekCorp <93953948+ElekCorp@users.noreply.github.com>
2022-10-25 16:08:11 +0200

    Add files via upload

---
+++ main.zig
@@ -0,0 +1,128 @@
+//The Computer Language Benchmarks Game
+// https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+ 
+// Contributed by Peter Elek
+ 
+// Algorithm lifted from C gcc #3 code by Mr Ledrug et al.
+
+
+const std = @import("std");
+
+pub fn A(i:u32,j:u32) f64
+{
+    return @intToFloat(f64,(i+j)*(i+j+1)/2+i+1);
+}
+
+pub fn dot(v: *[]f64, u: *[]f64, num: u32) f64
+{
+    var ind: u32 = 0;
+    var sum: f64 = 0;
+
+    while(ind<num):(ind+=1)
+    {
+        sum+=v.*[ind]*u.*[ind];
+    }
+
+    return sum;
+
+}
+
+pub fn mult_Av(in:*[]f64, out:*[]f64, n: u32) void
+{
+    var i: u32 =0;
+    var j: u32 =0;
+
+    while(i<n):(i+=1)
+    {
+    var sum: f64 =0;
+    j=0;
+        while(j<n):(j+=1)
+        {
+            sum+=in.*[j]/A(i,j);
+        }
+        out.*[i]=sum;
+    }
+}
+
+pub fn mult_Atv(in:*[]f64, out:*[]f64,n: u32) void
+{
+    var i: u32 =0;
+    var j: u32 =0;
+
+    while(i<n):(i+=1)
+    {
+    var sum: f64 =0;
+    j=0;
+        while(j<n):(j+=1)
+        {
+            sum+=in.*[j]/A(j,i);
+        }
+        out.*[i]=sum;
+    }
+}
+
+pub fn mult_AtAv(in:*[]f64,out:*[]f64,num:u32,tmp:*[]f64) void
+{
+    mult_Av(in,tmp,num);
+    mult_Atv(tmp,out,num);
+}
+
+
+
+pub fn main() !void 
+{
+    const c = @cImport
+(
+    {
+        @cInclude("stdlib.h");
+    }
+);
+
+
+     //set allocator
+     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+     defer _ = gpa.deinit();
+     const allocator = gpa.allocator();
+    
+    //set stdout
+    const stdout = std.io.getStdOut().writer();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    var num: u32=@intCast(u32,c.atoi(args[1]));
+
+    if (num <= 0) 
+    {
+        num = 2000;
+    }
+
+     //try stdout.print("matrix_size:{d}\n", .{num});
+     
+
+     //alloc memory
+     var u: []f64 = try allocator.alloc(f64, num);
+     var v: []f64 = try allocator.alloc(f64, num);
+     var tmp:[]f64 = try allocator.alloc(f64, num);
+
+     // frees at the end of the block
+     defer allocator.free(u);
+     defer allocator.free(v);
+     defer allocator.free(tmp);
+
+     var ind: u32 =0; 
+     while(ind<num):(ind+=1)
+     {
+        u[ind]=1;
+     }
+     ind=0;
+     while(ind<10):(ind+=1)
+     {
+        mult_AtAv(&u, &v, num, &tmp);
+        mult_AtAv(&v, &u, num, &tmp);
+     }
+
+     try stdout.print("{d:.9}\n",.{std.math.sqrt(dot(&u, &v, num) / dot(&v, &v, num))});
+
+     
+}
